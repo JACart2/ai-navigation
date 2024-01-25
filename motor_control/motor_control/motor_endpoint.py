75e9288f927521e54357fa48038fd2aaf997b755
Zane Metz <metzza@dukes.jmu.edu>
2024-01-25 17:12:09 -0500

    feat: Add constants to motor class

--- motor_control/motor_control/motor_endpoint.py
+++ motor_control/motor_control/motor_endpoint.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+import serial
+
+
+
+class MotorEndpoint(object):
+
+    def __init__(self):
+        #constants
+        self.BRAKE_TIME = 3
+        self.NODE_RATE = 10
+        self.STEERING_TOLERANCE = 50

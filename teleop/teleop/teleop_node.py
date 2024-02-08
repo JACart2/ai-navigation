02e7c8cb218e355af01f54f4d6f19e7d40680059
Zane Metz <zanem@cox.net>
2024-02-08 17:18:10 -0500

    feat: Convert message to correct type in teleop

--- teleop/teleop/teleop_node.py
+++ teleop/teleop/teleop_node.py
@@ -30,6 +30,7 @@ e = """
 Communications Failed
 """
 
+
 class Teleop(rclpy.node.Node):
     def __init__(self):
         super().__init__("teleop")
@@ -40,35 +41,38 @@ class Teleop(rclpy.node.Node):
         self.timer = self.create_timer(10, self.timer_callback)
 
         self.status = 0
-        self.target_linear_vel   = 0.0
-        self.target_angular_vel  = 0.0
-        self.control_linear_vel  = 0.0
+        self.target_linear_vel = 0.0
+        self.target_angular_vel = 0.0
+        self.control_linear_vel = 0.0
         self.control_angular_vel = 0.0
-    
+
     def getKey(self):
         tty.setraw(sys.stdin.fileno())
         rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
         if rlist:
             key = sys.stdin.read(1)
         else:
-            key = ''
+            key = ""
 
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
         return key
-    
+
     def vels(self, target_linear_vel, target_angular_vel):
-        return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel,target_angular_vel)
+        return "currently:\tlinear vel %s\t angular vel %s " % (
+            target_linear_vel,
+            target_angular_vel,
+        )
 
     def makeSimpleProfile(self, output, input, slop):
         if input > output:
-            output = min( input, output + slop )
+            output = min(input, output + slop)
         elif input < output:
-            output = max( input, output - slop )
+            output = max(input, output - slop)
         else:
             output = input
 
         return output
-    
+
     def constrain(self, input, low, high):
         if input < low:
             input = low
@@ -82,7 +86,7 @@ class Teleop(rclpy.node.Node):
     def checkLinearLimitVelocity(self, vel):
         vel = self.constrain(vel, -MAX_LIN_VEL, MAX_LIN_VEL)
         return vel
-    
+
     def checkAngularLimitVelocity(self, vel):
         vel = self.constrain(vel, -MAX_ANG_VEL, MAX_ANG_VEL)
         return vel
@@ -91,53 +95,66 @@ class Teleop(rclpy.node.Node):
         try:
             print(msg)
             key = self.getKey()
-            if key == 'w' :
-                target_linear_vel = self.checkLinearLimitVelocity(target_linear_vel + LIN_VEL_STEP_SIZE)
+            if key == "w":
+                target_linear_vel = self.checkLinearLimitVelocity(
+                    target_linear_vel + LIN_VEL_STEP_SIZE
+                )
                 status = status + 1
-                print(self.vels(target_linear_vel,target_angular_vel))
-            elif key == 'x' :
-                target_linear_vel = self.checkLinearLimitVelocity(target_linear_vel - LIN_VEL_STEP_SIZE)
+                print(self.vels(target_linear_vel, target_angular_vel))
+            elif key == "x":
+                target_linear_vel = self.checkLinearLimitVelocity(
+                    target_linear_vel - LIN_VEL_STEP_SIZE
+                )
                 status = status + 1
-                print(self.vels(target_linear_vel,target_angular_vel))
-            elif key == 'a' :
-                target_angular_vel = self.checkAngularLimitVelocity(target_angular_vel + ANG_VEL_STEP_SIZE)
+                print(self.vels(target_linear_vel, target_angular_vel))
+            elif key == "a":
+                target_angular_vel = self.checkAngularLimitVelocity(
+                    target_angular_vel + ANG_VEL_STEP_SIZE
+                )
                 status = status + 1
-                print(self.vels(target_linear_vel,target_angular_vel))
-            elif key == 'd' :
-                target_angular_vel = self.checkAngularLimitVelocity(target_angular_vel - ANG_VEL_STEP_SIZE)
+                print(self.vels(target_linear_vel, target_angular_vel))
+            elif key == "d":
+                target_angular_vel = self.checkAngularLimitVelocity(
+                    target_angular_vel - ANG_VEL_STEP_SIZE
+                )
                 status = status + 1
-                print(self.vels(target_linear_vel,target_angular_vel))
-            elif key == ' ' or key == 's' :
-                target_linear_vel   = 0.0
-                control_linear_vel  = 0.0
-                target_angular_vel  = 0.0
+                print(self.vels(target_linear_vel, target_angular_vel))
+            elif key == " " or key == "s":
+                target_linear_vel = 0.0
+                control_linear_vel = 0.0
+                target_angular_vel = 0.0
                 control_angular_vel = 0.0
                 print(self.vels(target_linear_vel, target_angular_vel))
             else:
-                if (key == '\x03'):
+                if key == "\x03":
                     self.destroy_timer(self.timer)
+                    return
 
             if status == 20:
                 print(msg)
                 status = 0
 
-            twist = Twist()
+            cmd = VelAnglePlanned()
 
-            control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
-            twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
+            control_linear_vel = self.makeSimpleProfile(
+                control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE / 2.0)
+            )
 
-            control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
-            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+            control_angular_vel = self.makeSimpleProfile(
+                control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE / 2.0)
+            )
 
-            self.nav_pub.publish(twist)
+            cmd.vel_planned = control_linear_vel
+            cmd.angle_planned = control_angular_vel
+            self.nav_pub.publish(cmd)
         except:
             print(e)
 
         finally:
-            twist = Twist()
-            twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
-            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-            pub.publish(twist)
+            cmd = VelAnglePlanned()
+            cmd.vel_planned = 0.0
+            cmd.angle_planned = 0.0
+            self.nav_pub.publish(cmd)
 
 
 def main():

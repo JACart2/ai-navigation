15431c27b3c2f888195d10d37699c399f0e7c6a0
Zane Metz <135646672+MillionDlr01@users.noreply.github.com>
2024-02-18 17:18:30 -0500

    feat: Add launch file to setup for motor controller

--- motor_control/setup.py
+++ motor_control/setup.py
@@ -9,6 +9,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=["setuptools"],
     zip_safe=True,

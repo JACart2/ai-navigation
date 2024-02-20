db89021caa1bb4de26459c6e652d33808be7bdc4
Zane Metz <zanem@cox.net>
2024-02-20 16:32:40 -0500

    fix: fix share dir

--- motor_control/setup.py
+++ motor_control/setup.py
@@ -9,7 +9,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # ("share/motor_control/launch", "launch/motor.launch.py"),
+        # (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=["setuptools"],
     zip_safe=True,

e48b3dbee90e338ee47aeb47193f62ed45fba73f
Zane Metz <metzza@dukes.jmu.edu>
2024-04-03 17:51:00 -0400

    feat: Add launch file for simulator

--- navigation/launch/sim_nav.launch.py
+++ navigation/launch/sim_nav.launch.py
@@ -1,15 +1,29 @@
 """ Launch the motor control system.
 """
 
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     return LaunchDescription(
         [
+            Node(
+                package="rviz2",
+                namespace="",
+                executable="rviz2",
+                name="rviz2",
+                arguments=[
+                    "-d",
+                    os.path.join(
+                        get_package_share_directory("navigation"),
+                        "rviz/visualize_path.rviz",
+                    ),
+                ],
+            ),
             DeclareLaunchArgument(
                 "graph_file",
                 default_value="/home/student/dev_ws/src/ai-navigation/navigation/navigation/drive_build.gml",
@@ -23,6 +37,14 @@ def generate_launch_description():
                 package="navigation",
                 executable="local_planner",
                 output="screen",
+            ),
+            Node(
+                package="navigation",
+                executable="motor_simulator",
+            ),
+            Node(
+                package="navigation",
+                executable="global_tester",
             ),
         ]
     )
--- navigation/setup.py
+++ navigation/setup.py
@@ -15,6 +15,10 @@ setup(
             os.path.join("share", package_name, "launch"),
             glob(os.path.join("launch", "*launch.[pxy][yma]*")),
         ),
+        (
+            os.path.join("share", package_name, "rviz"),
+            glob(os.path.join("test", "*.rviz")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,

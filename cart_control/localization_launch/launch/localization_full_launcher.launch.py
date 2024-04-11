01395a7390d78e30d6fe4b2fb6845d4786306740
WalkerTodd <walkertodd03@gmail.com>
2024-04-11 14:10:48 -0400

    Localization Launch File working?

--- cart_control/localization_launch/launch/localization_full_launcher.launch.py
+++ cart_control/localization_launch/launch/localization_full_launcher.launch.py
@@ -1,21 +1,10 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
-    # Declare the use_imu launch argument
-    use_imu_arg = DeclareLaunchArgument(
-        'use_imu', default_value='false',
-        description='Whether to use IMU data or not.'
-    )
-    use_whatever = DeclareLaunchArgument(
-        'use_whatever', default_value='false',
-        description='Whether to use whatever data or not.'
-    )
-
     # Launch the velodyne_driver node for VLP16
     velodyne_driver_node = Node(
         package='velodyne_driver',
@@ -35,13 +24,12 @@ def generate_launch_description():
         }],
     )
 
-    # Include the lidar_localization launch file directly, with dynamic use_imu parameter
+    # Include the lidar_localization launch file directly
     lidar_localization_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             FindPackageShare('lidar_localization_ros2'),
             '/launch/lidar_localization.launch.py'
-        ]),
-        launch_arguments={'use_imu': LaunchConfiguration('use_imu')}.items(),
+        ])
     )
 
     # Execute the RViz2 command with the specified configuration file
@@ -52,7 +40,6 @@ def generate_launch_description():
 
     # Combine all the above components into a single launch description
     return LaunchDescription([
-        use_imu_arg,
         velodyne_driver_node,
         velodyne_transform_node,
         lidar_localization_launch,

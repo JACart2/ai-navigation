446798189e127ed97d161c9fb2d2591e6def44cc
WalkerTodd <walkertodd03@gmail.com>
2024-07-04 12:58:14 -0400

    This is fun

--- navigation/launch/sim_nav.launch.py
+++ navigation/launch/sim_nav.launch.py
@@ -1,3 +1,6 @@
+""" Launch the motor control system.
+"""
+
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -26,37 +29,13 @@ def generate_launch_description():
                 "graph_file",
                 default_value="./src/ai-navigation/navigation/maps/main_shift3.gml",
             ),
-            DeclareLaunchArgument(
-                "anchor_gps",
-                default_value="38.433939, -78.862157",
-            ),
-            DeclareLaunchArgument(
-                "anchor_theta",
-                default_value="100",
-            ),
-            DeclareLaunchArgument(
-                "anchor_local",
-                default_value="0, 0",
-            ),
-            DeclareLaunchArgument(
-                "test_loc_gps",
-                default_value="38.433170, -78.860981",
-            ),
-            DeclareLaunchArgument(
-                "test_loc_local",
-                default_value="67.6, 115",
-            ),
             Node(
-                package='navigation',
-                executable='global_planner',
-                output='screen',
+                package="navigation",
+                executable="global_planner",
+                output="screen",
                 parameters=[
                     {
-                        'anchor_local': LaunchConfiguration('anchor_local'),
-                        'test_loc_gps': LaunchConfiguration('test_loc_gps'),
-                        'test_loc_local': LaunchConfiguration('test_loc_local'),
-                        'anchor_gps': LaunchConfiguration('anchor_gps'),
-                        'anchor_theta': LaunchConfiguration('anchor_theta'),
+                        "graph_file": LaunchConfiguration("graph_file"),
                     }
                 ],
             ),
@@ -68,43 +47,20 @@ def generate_launch_description():
             Node(
                 package="navigation",
                 executable="motor_simulator",
-                output="screen",
             ),
             Node(
                 package="navigation",
                 executable="global_tester",
-                output="screen",
             ),
             Node(
                 package="navigation",
                 executable="visualize_graph",
-                output="screen",
                 parameters=[
                     {
                         "graph_file": LaunchConfiguration("graph_file"),
-
                     }
                 ],
             ),
-            gps_util_params = {
-            'anchor_gps': [38.433939, -78.862157],
-            'anchor_theta': 100,
-            'anchor_local': [0, 0],
-            'test_loc_gps': [38.433170, -78.860981],
-            'test_loc_local': [67.6, 115],
-            'max_speed': 5,
-            'max_ndt_health': 4,
-            'min_obstacle_dist': 0.5,
-            'min_obstacle_time': 0.5,
-            'safe_obstacle_dist': 6,
-            'safe_obstacle_time': 2,
-            'vehicle_width': 1.1938,
-            'vehicle_length': 2.4003,
-            'wheel_base': 2.4003,
-            'front_axle_track': 0.9017,
-            'rear_axle_track': 0.9652,
-            'tire_width': 0.2159
-        }
         ]
     )
 

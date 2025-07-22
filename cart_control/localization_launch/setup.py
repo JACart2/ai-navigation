ece4b7115048e7db8302db76beb6f357336a88b8
Augusto Fernandez-Circelli <100038829+augustodfc@users.noreply.github.com>
2025-07-22 09:27:32 -0400

    Update setup.py
    Allows ZED config to be reflected when used

--- cart_control/localization_launch/setup.py
+++ cart_control/localization_launch/setup.py
@@ -14,7 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), 
-        glob('launch/*.launch.py')),
+        glob('launch/*.launch.py') + glob('launch/*.yaml')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],

1e948d9bf31d26d1444484c5c0900809a30b9750
Hunter Fauntleroy <fauntlht@dukes.jmu.edu>
2026-03-05 17:49:51 +0000

    pulled and merged main into branch

--- install/localization_launch/share/localization_launch/launch/localization_full_launcher.launch.py
+++ install/localization_launch/share/localization_launch/launch/localization_full_launcher.launch.py
@@ -1,8 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import launch_ros
 import launch_ros.actions
@@ -11,6 +12,7 @@ import os
 
 
 def generate_launch_description():
+    cart_config_path = LaunchConfiguration("cart_config_path")
 
     # Launch the velodyne_driver node for VLP16
     velodyne_driver_node = Node(
@@ -68,12 +70,19 @@ def generate_launch_description():
 
     # Include the cameras launch file
     cameras_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([cameras_launch_path])
+        PythonLaunchDescriptionSource([cameras_launch_path]),
+        launch_arguments={
+            "cart_config_path": cart_config_path,
+        }.items(),
     )
 
     # Combine all the above components into a single launch description
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                "cart_config_path",
+                description="Path to cart-specific YAML config (must contain zed_front_serial and zed_rear_serial)",
+            ),
             velodyne_driver_node,
             velodyne_transform_launch,
             lidar_localization_launch,

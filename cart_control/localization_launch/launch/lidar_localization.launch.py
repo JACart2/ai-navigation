ad70c46ba60592d1d9b5b85da7d739ba1a400960
Gr8jj28 <gr8jj28@gmail.com>
2026-05-09 17:20:55 -0400

    codex init attempt

--- cart_control/localization_launch/launch/lidar_localization.launch.py
+++ cart_control/localization_launch/launch/lidar_localization.launch.py
@@ -8,10 +8,6 @@ import launch_ros
 import launch_ros.actions
 import launch_ros.events
 
-from launch import LaunchDescription
-from launch_ros.actions import LifecycleNode
-from launch_ros.actions import Node
-
 import lifecycle_msgs.msg
 
 from ament_index_python.packages import get_package_share_directory
@@ -27,13 +23,6 @@ def generate_launch_description():
         package="tf2_ros",
         executable="static_transform_publisher",
         arguments=["1", "0", "1.9", "0", "0", "0", "1", "base_link", "velodyne"],
-    )
-
-    imu_tf = launch_ros.actions.Node(
-        name="imu_tf",
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        arguments=["0", "0", "0", "0", "0", "0", "1", "base_link", "imu_link"],
     )
 
     # Set the default path directly to the specific YAML file location
@@ -58,13 +47,6 @@ def generate_launch_description():
             ("/imu", "/zed/zed_node/imu/data"),
         ],
         output="screen",
-    )
-
-    to_inactive = launch.actions.EmitEvent(
-        event=launch_ros.events.lifecycle.ChangeState(
-            lifecycle_node_matcher=launch.events.matches_action(lidar_localization),
-            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
-        )
     )
 
     from_unconfigured_to_inactive = launch.actions.RegisterEventHandler(
@@ -109,6 +91,5 @@ def generate_launch_description():
 
     ld.add_action(lidar_localization)
     ld.add_action(lidar_tf)
-    ld.add_action(to_inactive)
 
     return ld

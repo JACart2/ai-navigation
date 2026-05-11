61fdf336ab855fa98406bcff0877f4862dd1a74b
Gr8jj28 <gr8jj28@gmail.com>
2026-05-11 14:07:50 -0400

    update no map seen

--- cart_control/localization_launch/launch/lidar_localization.launch.py
+++ cart_control/localization_launch/launch/lidar_localization.launch.py
@@ -49,21 +49,10 @@ def generate_launch_description():
         output="screen",
     )
 
-    from_unconfigured_to_inactive = launch.actions.RegisterEventHandler(
-        launch_ros.event_handlers.OnStateTransition(
-            target_lifecycle_node=lidar_localization,
-            goal_state="unconfigured",
-            entities=[
-                launch.actions.LogInfo(msg="-- Unconfigured --"),
-                launch.actions.EmitEvent(
-                    event=launch_ros.events.lifecycle.ChangeState(
-                        lifecycle_node_matcher=launch.events.matches_action(
-                            lidar_localization
-                        ),
-                        transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
-                    )
-                ),
-            ],
+    to_inactive = launch.actions.EmitEvent(
+        event=launch_ros.events.lifecycle.ChangeState(
+            lifecycle_node_matcher=launch.events.matches_action(lidar_localization),
+            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
         )
     )
 
@@ -86,10 +75,10 @@ def generate_launch_description():
         )
     )
 
-    ld.add_action(from_unconfigured_to_inactive)
     ld.add_action(from_inactive_to_active)
 
     ld.add_action(lidar_localization)
     ld.add_action(lidar_tf)
+    ld.add_action(to_inactive)
 
     return ld

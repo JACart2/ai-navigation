5fc951a1f77ad919f294ce3052ca3d4c733ef7e0
Jake Dinh <dinhjx@dukes.jmu.edu>
2026-07-08 15:35:18 -0400

    zed camera now lanches with mola_autonomy.launch.py

--- cart_control/localization_launch/launch/zed_camera.launch.py
+++ cart_control/localization_launch/launch/zed_camera.launch.py
@@ -134,6 +134,7 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             config_common_path,
             config_camera_path,
+            *extra_param_files,
             {
                 'use_sim_time': use_sim_time,
                 'simulation.sim_enabled': sim_mode,
@@ -149,13 +150,10 @@ def launch_setup(context, *args, **kwargs):
                 'pos_tracking.publish_map_tf': publish_map_tf,
                 'sensors.publish_imu_tf': publish_imu_tf,
                 'general.pub_frame_rate': 15.0,
-                'pos_tracking.publish_tf': publish_tf,
-                'pos_tracking.publish_map_tf': publish_map_tf,
-                'sensors.publish_imu_tf': publish_imu_tf,
                 'object_detection.od_enabled': True,
-                'object_detection.model': 'MULTI_CLASS_BOX_FAST',
+                'object_detection.detection_model': 'MULTI_CLASS_BOX_FAST',
+                'object_detection.filtering_mode': 'NMS3D',
             },
-            *extra_param_files,
         ],
     )
 
